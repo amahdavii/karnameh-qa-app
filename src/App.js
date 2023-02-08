f56aa2524e3d941436f29d98ba0271e82daf8b1c
Arian Mahdavi <amahdavi9978@gmail.com>
2023-02-08 20:15:40 +0330

    Added react router config.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import { RouterProvider } from "react-router-dom"
+import { router } from "@/routes"
+
 function App() {
-  return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold">سلام دنیا!</h1>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom"
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: "",
+    children: [
+      {
+        path: "",
+        element: "",
+      },
+      {
+        path: "",
+        element: "",
+      },
+    ],
+  },
+])
